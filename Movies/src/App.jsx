81c787e85faffefb4d8f8c72b992d2242bbd55ab
Ashish Jadhav <130998599+2022ashishj@users.noreply.github.com>
2024-11-16 16:07:24 +0530

    Update App.jsx

--- Movies/src/App.jsx
+++ Movies/src/App.jsx
@@ -1,11 +1,8 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import MovieSearchApp from './components/MovieSearchApp'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
